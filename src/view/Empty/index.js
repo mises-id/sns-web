a6e1553ef77800b8d2a955ded13c3d0eb5507ac0
lmk <1056509662@qq.com>
2021-08-18 22:45:11 +0800

    fix bug

--- src/view/Empty/index.js
+++ src/view/Empty/index.js
@@ -1,9 +1,9 @@
 /*
  * @Author: lmk
  * @Date: 2021-08-18 22:36:01
- * @LastEditTime: 2021-08-18 22:36:18
+ * @LastEditTime: 2021-08-18 22:44:58
  * @LastEditors: lmk
- * @Description: empty
+ * @Description: empty page
  */
 import React from 'react';
 const Empty = (props)=>{
