c3b64f96151801f97f1a6cd5ef6257763fb06bc7
liskk <1056509662@qq.com>
2022-03-24 13:13:35 +0800

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: lmk
  * @Date: 2021-07-07 23:23:36
- * @LastEditTime: 2022-02-08 09:39:16
+ * @LastEditTime: 2022-03-24 13:10:07
  * @LastEditors: lmk
  * @Description: 
  */
@@ -13,7 +13,20 @@ import reportWebVitals from './reportWebVitals';
 import './locales'
 // import VConsole from 'vconsole';
 import MisesExtensionController from  './utils/postMessage'
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 window.mises = new MisesExtensionController()
+
+Sentry.init({
+  dsn: "https://ce70d202b4be4f7685dbf1ed40a55227@o1162849.ingest.sentry.io/6274250",
+  integrations: [new BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+
 render(
   <React.StrictMode>
     <App />
