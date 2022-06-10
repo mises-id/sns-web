554628c866a2f59f65e53a209a9d03be6582cc11
liskk <1056509662@qq.com>
2022-06-11 00:35:33 +0800

    fix: minus comment popup height

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,17 +1,15 @@
 /*
  * @Author: lmk
  * @Date: 2021-08-07 22:30:53
- * @LastEditTime: 2022-06-11 00:06:41
+ * @LastEditTime: 2022-06-11 00:32:16
  * @LastEditors: lmk
  * @Description:
  */
 import React from "react";
-import { NavBar } from "antd-mobile";
+import { NavBar } from "zarm";
 import backIcon from "@/images/back.png";
-import './index.scss'
-// import { useHistory } from "react-router-dom";
-const Navbar = ({ title,rightChild,fixed=false }) => {
-  // const history = useHistory()
+
+const Navbar = ({ title,rightBtnClick,rightChild }) => {
   const back = () => {
     if (window.history.length === 1) {
       window.location.replace("/home/discover");
@@ -20,16 +18,18 @@ const Navbar = ({ title,rightChild,fixed=false }) => {
     }
   };
   return (
-    <NavBar onBack={back}
-      className={fixed ? "fixed-navbar" : ""}
-      backArrow={<img src={backIcon} alt="" width={22} height={22} style={{display:'block'}}/>}
-      right={rightChild&&<div className="m-flex m-row-end">{rightChild}</div>}>
-      <span style={{
-        fontSize: '19px',
-        fontWeight: 'bold',
-        color:'#333'
-      }}>{title}</span>
-    </NavBar>
+    <NavBar
+      left={
+        <img
+          src={backIcon}
+          alt="back"
+          onClick={back}
+          style={{ width: "22px", height: "22px" }}
+        />
+      }
+      title={title}
+      right={rightChild}
+    />
   );
 };
 export default Navbar;
