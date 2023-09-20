6486a8fd6c968e859aa19e56eb7dd050ba7fe456
liskk <1056509662@qq.com>
2023-09-20 15:13:50 +0800

    fix: fix docs

--- src/view/Me/index.js
+++ src/view/Me/index.js
@@ -279,7 +279,7 @@ const Myself = ({ history }) => {
     return <div className="bonuses-container">
       <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="40" height="40"><path d="M640 224c19.2 0 38.4 9.6 51.2 25.6l118.4 156.8c19.2 25.6 16 57.6-3.2 80l-246.4 275.2c-22.4 25.6-64 28.8-89.6 6.4-3.2 0-3.2-3.2-3.2-3.2l-249.6-275.2c-19.2-22.4-22.4-57.6-3.2-83.2l118.4-156.8c12.8-16 32-25.6 51.2-25.6h256z m0 64h-256l-118.4 156.8 246.4 275.2 246.4-275.2L640 288z m-32 96c19.2 0 32 12.8 32 32s-12.8 32-32 32h-192c-19.2 0-32-12.8-32-32s12.8-32 32-32h192z" fill="#333333" data-spm-anchor-id="a313x.search_index.0.i0.72cd3a81Rm0qVB"></path></svg>
       <div className="flex-1 right-content">
-        <p className="bonuses-title">My Bonuses</p>
+        <p className="bonuses-title">My Reward Points</p>
         <p className="mb-10 bonuses-value">{bonusesCount}</p>
       </div>
       <div className="flex align-center">
